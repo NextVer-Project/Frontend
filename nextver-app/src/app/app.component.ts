5ab49412d3e976db4bb3c265ac59100e958fb3c7
Błażej Zajac <blazezaj@gmail.com>
2024-03-15 21:32:48 +0100

    Update AppModule and AppComponent

--- nextver-app/src/app/app.component.ts
+++ nextver-app/src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, AfterViewInit, HostListener } from '@angular/core';
+import { UIPresentationConfigService } from './services/ui-presentation-config.service';
+import { Router } from '@angular/router';
+import { Platform } from '@angular/cdk/platform';
+import { Pipe, PipeTransform } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'nextver-app';
+export class AppComponent implements OnInit {
+  title: string = 'nextver-app';
+  public shouldBeDropdown = false;
+
+
+  constructor(public router: Router, public platform: Platform, private uiPresentationConfigService: UIPresentationConfigService) { }
+
+  ngOnInit() {
+    const selectedTheme = this.uiPresentationConfigService.getSelectedTheme();
+    this.uiPresentationConfigService.setTheme(selectedTheme);
+
+    const animationEnabled = this.uiPresentationConfigService.getSelectedAnimation();
+    this.uiPresentationConfigService.toggleBoxAnimation(animationEnabled);
+
+    const scrollSnapEnabled = this.uiPresentationConfigService.getScrollSnapState();
+    this.uiPresentationConfigService.applyScrollSnap(scrollSnapEnabled);
+
+    this.resizeScreen();
+  }
+
+  public resizeScreen(): void {
+    const screenWidth = window.innerWidth;
+    this.shouldBeDropdown = screenWidth <= 1000 || this.platform.ANDROID || this.platform.IOS;
+  }
+
+  get selectedScrollSnapState() {
+    return this.uiPresentationConfigService.getScrollSnapState();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    if (this.selectedScrollSnapState) { // Sprawdź, czy Scroll Snap jest aktywny
+      this.uiPresentationConfigService.applyScrollSnap(true); // Wywołaj metodę applyScrollSnap tylko gdy Scroll Snap jest aktywny
+    }
+  }
+
+}
+
+
+@Pipe({ name: 'safe' })
+export class SafePipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) { }
+  transform(url) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
--- nextver-app/src/app/app.module.ts
+++ nextver-app/src/app/app.module.ts
@@ -1,16 +1,68 @@
-import { NgModule } from '@angular/core';
+import { NgModule,  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PlatformModule } from '@angular/cdk/platform';
 
-import { AppComponent } from './app.component';
+import { AppComponent, SafePipe } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { UIPresentationConfigService } from './services/ui-presentation-config.service';
+import { MovieComponent } from './components/movie/movie.component';
+import { TvshowComponent } from './components/tvshow/tvshow.component';
+import { ProductionBrowserComponent } from './components/productionbrowser/production-browser.component';
+import { ProductionRowComponent } from './components/productionbrowser/production-row/production-row.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { MovieDetailsComponent } from './components/movie/movie-details/movie-details.component';
+import { TvshowDetailsComponent } from './components/tvshow/tvshow-details/tvshow-details.component';
+import { MovieQualityVersionsDetailsComponent } from './components/movie/movie-details/movie-quality-versions-details/movie-quality-versions-details.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'movie', component: MovieComponent },
+  { path: 'browser', component: ProductionBrowserComponent },
+  { path: 'details/:category/:id', component: MovieDetailsComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    MovieComponent,
+    TvshowComponent,
+    ProductionBrowserComponent,
+    ProductionRowComponent,
+    RegisterComponent,
+    MovieDetailsComponent,
+    TvshowDetailsComponent,
+    SafePipe,
+    MovieQualityVersionsDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, {
+      onSameUrlNavigation: 'reload'
+    }),
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    PlatformModule
   ],
-  providers: [],
+  providers: [
+    UIPresentationConfigService,
+    DatePipe
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
