9ec41278c40e0f004920f67592a3a46b79348670
Błażej Zajac <blazezaj@gmail.com>
2024-05-29 20:22:38 +0200

    Update ui-presentation-config.service.spec.ts

--- nextver-app/src/app/services/ui-presentation-config.service.spec.ts
+++ nextver-app/src/app/services/ui-presentation-config.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UIPresentationConfigService } from './theme.service';
+import { UIPresentationConfigService } from './ui-presentation-config.service';
 
 describe('ThemeService', () => {
   let service: UIPresentationConfigService;
