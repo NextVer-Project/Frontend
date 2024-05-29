997036a3fc90fd71f982f7f6c9ac0c085c24bff2
Błażej Zajac <blazezaj@gmail.com>
2024-05-29 20:23:38 +0200

    Update technology.service.ts

--- nextver-app/src/app/api/technology.service.ts
+++ nextver-app/src/app/api/technology.service.ts
@@ -1,16 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
-import { TvShowDto } from './dtos/tvshow.dto';
-import { MovieDto } from './dtos/movie.dto';
 import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
 import { ApiPaths, environment } from '../../environments/environment';
-import { catchError, map } from 'rxjs/operators';
-import { PaginatedResult } from './dtos/pagination';
 import { AuthService } from './auth.service';
-import { ProductionCountDto } from "./dtos/production-count.dto";
-import { ProductionDto } from './dtos/production.dto';
-import { ReleasePlaceForEditDto } from './dtos/release-place-for-edit.dto';
-import { ProductionTypeForEditDto } from './dtos/production-type-for-edit.dto';
 import { TechnologyForEditDto } from './dtos/technology-for-edit.dto';
 
 @Injectable({
