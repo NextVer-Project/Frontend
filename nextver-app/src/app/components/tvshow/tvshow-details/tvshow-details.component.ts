413aa36d7b7865407ca3ea2bd1156ed446d9c28d
Błażej Zajac <blazezaj@gmail.com>
2024-05-29 21:08:29 +0200

    Add TvShow component

---
+++ nextver-app/src/app/components/tvshow/tvshow-details/tvshow-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tvshow-details',
+  templateUrl: './tvshow-details.component.html',
+  styleUrls: ['./tvshow-details.component.css']
+})
+export class TvshowDetailsComponent {
+
+}
